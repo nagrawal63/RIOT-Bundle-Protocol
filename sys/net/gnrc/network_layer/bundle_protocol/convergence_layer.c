37c9016c30de4f4cc4fe4cd6a65a56fd568b0011
Nishchay-sopho <agrawal.nishchay5@gmail.com>
2020-04-26 03:20:09 +0200

    testing

--- sys/net/gnrc/network_layer/bundle_protocol/convergence_layer.c
+++ sys/net/gnrc/network_layer/bundle_protocol/convergence_layer.c
@@ -124,6 +124,7 @@ bool is_packet_ack(gnrc_pktsnip_t *pkt) {
   char temp[ACK_IDENTIFIER_SIZE];
   od_hex_dump(pkt->data, pkt->size, OD_WIDTH_DEFAULT);
   strncpy(temp, pkt->data, ACK_IDENTIFIER_SIZE);
+  printf("convergence_layer: string in is_packet_ack: %s.\n", temp);
   if (strcmp(temp, "ack") == 0) {
     return true;
   }
